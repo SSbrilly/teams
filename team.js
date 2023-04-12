048f07bea21366c18b11a5a2785afedfbb7145aa
Mirka <mirkamedaglini@hotmail.it>
2023-04-12 18:23:11 +0200

    Mirka

--- team.js
+++ team.js
@@ -11,4 +11,17 @@ const team = [
     favoriteBook: 'Game OF Thrones',
     petName: 'Dylan, Black, Lucky',
   },
+    {
+    name: "Mirka",
+    surname: "Medaglini",
+    age: "25",
+    city: "Lucca",
+    hobby: "Music",
+    favoriteFood: "Pizza",
+    favoriteVideoGame: "Super Mario Odyssey",
+    favoriteFilm: "Eternal sunshine of the spotless mind",
+    favoriteBook: "Il maestro e Margherita",
+    petName: "Nikita",
+  },
+
 ];
