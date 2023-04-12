f5b713cc01898654942f42eead9e262c10084760
Antonino Alampi <Alampiantonino99@gmail.com>
2023-04-12 17:43:13 +0200

    Aggiunti dati

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const team = [{}];
+const team = [
+    {
+        name: "Antonino",
+        surname: "Tony",
+        age: 23,
+        city: "Reggio di Calabria",
+        hobby: "",
+        favoriteFood: "Sushi",
+        favoriteVideoGame: "Dark Souls",
+        favoriteFilm: "Harry Potter",
+        favoriteBook: "",
+        petName: ""
+    }
+];
